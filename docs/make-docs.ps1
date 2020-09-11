398dc76328e94db0f0631e98d229e500656102ab
tmontaigu <thomas.montaigu@laposte.net>
2020-09-11 16:03:05 +0200

    powershell script to make docs

--- docs/make-docs.ps1
+++ docs/make-docs.ps1
@@ -8,8 +8,14 @@ param(
 $PluginDir = Join-Path -Resolve $InstallDir "plugins/Python/Lib/site-packages/"
 if ($null -eq $env:pythonpath) {
     $env:pythonpath = $PluginDir
-} elseif ($env:pythonpath.indexOf($PluginDir) -eq -1) {
+} elseif ($env:pythonpath.IndexOf($PluginDir) -eq -1) {
     $env:pythonpath = ";$PluginDir"
 }
+
+if ($env:path.IndexOf($InstallDir) -eq -1) {
+    $env:path += ";$InstallDir"
+    $env:path += ";$(Join-Path $InstallDir plugins)"
+}
+
 
 sphinx-build -b $BuildType . $OutputDir
