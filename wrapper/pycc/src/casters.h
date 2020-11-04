6691cb67d95ab036f3a0e43de6c3b4c5ccfad397
tmontaigu <thomas.montaigu@laposte.net>
2020-11-04 20:50:43 +0100

    Add license header everywhere

--- wrapper/pycc/src/casters.h
+++ wrapper/pycc/src/casters.h
@@ -1,6 +1,19 @@
-//
-// Created by Thomas on 09/06/2020.
-//
+//##########################################################################
+//#                                                                        #
+//#                CLOUDCOMPARE PLUGIN: PythonPlugin                       #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#                   COPYRIGHT: Thomas Montaigu                           #
+//#                                                                        #
+//##########################################################################
 
 #ifndef CLOUDCOMPAREPROJECTS_CASTERS_H
 #define CLOUDCOMPAREPROJECTS_CASTERS_H
