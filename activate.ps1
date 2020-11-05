2687c66cb56a54c954a001da0e692a2b0eb4fdba
tmontaigu <thomas.montaigu@laposte.net>
2020-11-05 10:09:09 +0100

    remove activate script

--- activate.ps1
+++
@@ -1,8 +0,0 @@
-#$env:CONDA_PREFIX = "C:\ProgramData\Miniconda3\envs\pyccenv";
-
-$env:pythonhome = "$env:CONDA_PREFIX"
-$env:pythonpath += ";$( Join-Path $env:pythonhome DLLs )"
-$env:pythonpath += ";$( Join-Path $env:pythonhome  Lib\site-packages )"
-$env:pythonpath += ";C:\Users\t.montaigu\Projects\CloudCompare\build\release\plugins\private\CloudCompare-PythonPlugin\wrapper"
-
-
