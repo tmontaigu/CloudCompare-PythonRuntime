e402b3e9cf9d7dde1ffae0eef42be76afe727571
tmontaigu <thomas.montaigu@laposte.net>
2020-11-16 11:33:54 +0100

    Add example script that hides then shows entities with a delay

---
+++ script_examples/hide_show_entities.py
@@ -0,0 +1,29 @@
+""" Hides all Selected entities, and re-shows them with a delay 
+"""
+import cccorelib
+import pycc
+
+from time import sleep
+
+cc = pycc.GetInstance()
+
+
+entities = cc.getSelectedEntities()
+
+for entity in entities:
+    entity.setVisible(False)
+
+cc.updateUI()
+cc.redrawAll()
+
+pycc.RunInThread(sleep, 2)
+
+for entity in entities:
+    entity.setVisible(True)
+
+    cc.updateUI()
+    cc.redrawAll()
+
+    print(f"{entity} shown")
+    pycc.RunInThread(sleep, 5)
+
