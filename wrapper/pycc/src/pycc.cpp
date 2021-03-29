e3fd7f8e091cae224bf56ba3be3571292048aa3f
tmontaigu <thomas.montaigu@laposte.net>
2021-03-29 13:17:59 +0200

    move threading specific things into threading.h

--- wrapper/pycc/src/pycc.cpp
+++ wrapper/pycc/src/pycc.cpp
@@ -43,6 +43,7 @@
 #include "Runtime.h"
 #include "casters.h"
 #include "ccGUIPythonInstance.h"
+#include "threading.h"
 
 #include <FileIOFilter.h>
 #include <ccCommandLineInterface.h>
@@ -56,80 +57,7 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-class PyThreadStateGuard
-{
-  public:
-    explicit PyThreadStateGuard(PyInterpreterState *interpState)
-    {
-        pCurrentState = PyThreadState_New(interpState);
-        PyEval_AcquireThread(pCurrentState);
-    }
 
-    virtual ~PyThreadStateGuard()
-    {
-        PyThreadState_Clear(pCurrentState);
-        PyEval_ReleaseThread(pCurrentState);
-
-        PyThreadState_Delete(pCurrentState);
-    }
-
-  private:
-    PyThreadState *pCurrentState{nullptr};
-};
-
-struct PyThreadStateReleaser
-{
-    explicit PyThreadStateReleaser() : state(PyEval_SaveThread()) {}
-
-    virtual ~PyThreadStateReleaser()
-    {
-        PyEval_RestoreThread(state);
-    }
-
-    PyThreadState *state{nullptr};
-};
-
-class MyException : public QException
-{
-  public:
-    explicit MyException(const std::exception &err) : e(err) {}
-
-    void raise() const override
-    {
-        throw *this;
-    }
-
-    QException *clone() const override
-    {
-        return new MyException(*this);
-    }
-
-    const char *what() const noexcept override
-    {
-        return e.what();
-    }
-
-    std::exception error() const
-    {
-        return e;
-    }
-
-  private:
-    std::exception e;
-};
-
-py::object call_fn(PyThreadState *main_state, py::object callable, py::args args, py::kwargs kwargs)
-{
-    PyThreadStateGuard threadStateGuard{main_state->interp};
-    try
-    {
-        return callable(*args, **kwargs);
-    }
-    catch (const std::exception &e)
-    {
-        throw MyException(e);
-    }
-}
 
 void define_ccCommandLine(py::module &);
 void define_ccDrawableObject(py::module &);
@@ -313,7 +241,7 @@ void define_pycc(py::module &m)
         loop.exec();
 
         return future.result();
-    });
+    }, "callable"_a);
 
     m.def("RunThread", [](py::object thread) {
         py::object isAliveMethod = thread.attr("is_alive");
---
+++ wrapper/pycc/src/threading.h
@@ -0,0 +1,100 @@
+//##########################################################################
+//#                                                                        #
+//#                CLOUDCOMPARE PLUGIN: PythonPlugin                       #
+//#                                                                        #
+//#  This program is free software; you can redistribute it and/or modify  #
+//#  it under the terms of the GNU General Public License as published by  #
+//#  the Free Software Foundation; version 2 of the License.               #
+//#                                                                        #
+//#  This program is distributed in the hope that it will be useful,       #
+//#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+//#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+//#  GNU General Public License for more details.                          #
+//#                                                                        #
+//#                   COPYRIGHT: Thomas Montaigu                           #
+//#                                                                        #
+//##########################################################################
+
+
+#ifndef CLOUD_COMPARE_PROJECTS_THREADING_H
+#define CLOUD_COMPARE_PROJECTS_THREADING_H
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+class PyThreadStateGuard
+{
+  public:
+    explicit PyThreadStateGuard(PyInterpreterState *interpState)
+    {
+        pCurrentState = PyThreadState_New(interpState);
+        PyEval_AcquireThread(pCurrentState);
+    }
+
+    virtual ~PyThreadStateGuard()
+    {
+        PyThreadState_Clear(pCurrentState);
+        PyEval_ReleaseThread(pCurrentState);
+
+        PyThreadState_Delete(pCurrentState);
+    }
+
+  private:
+    PyThreadState *pCurrentState{nullptr};
+};
+
+struct PyThreadStateReleaser
+{
+    explicit PyThreadStateReleaser() : state(PyEval_SaveThread()) {}
+
+    virtual ~PyThreadStateReleaser()
+    {
+        PyEval_RestoreThread(state);
+    }
+
+    PyThreadState *state{nullptr};
+};
+
+class ThreadException : public QException
+{
+  public:
+    explicit ThreadException(const std::exception &err) : e(err) {}
+
+    void raise() const override
+    {
+        throw *this;
+    }
+
+    QException *clone() const override
+    {
+        return new ThreadException(*this);
+    }
+
+    const char *what() const noexcept override
+    {
+        return e.what();
+    }
+
+    std::exception error() const
+    {
+        return e;
+    }
+
+  private:
+    std::exception e;
+};
+
+inline py::object call_fn(PyThreadState *main_state, py::object callable, py::args args, py::kwargs kwargs)
+{
+    PyThreadStateGuard threadStateGuard{main_state->interp};
+    try
+    {
+        return callable(*args, **kwargs);
+    }
+    catch (const std::exception &e)
+    {
+        throw ThreadException(e);
+    }
+}
+#endif // CLOUD_COMPARE_PROJECTS_THREADING_H
