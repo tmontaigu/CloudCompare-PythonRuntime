4acbf384eee2d8595c3ad9725581a03da2aab6cd
tmontaigu <thomas.montaigu@laposte.net>
2021-04-11 18:43:13 +0200

    Make simplification of the copying of the python env

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,83 +1,45 @@
-function(copy_python_venv install_dir)
-    if (CONDA_PREFIX)
-        INSTALL(
-                DIRECTORY "${CONDA_PREFIX}/"
-                DESTINATION "${install_dir}"
-                PATTERN "tests/*" EXCLUDE
-        )
-    elseif (VENV_PREFIX)
-        execute_process(
-            COMMAND 
-                "${PYTHON_EXECUTABLE}" 
-                "-c" 
-                "from distutils import sysconfig as s;print(s.BASE_PREFIX, end='');"
-            RESULT_VARIABLE _PYTHON_SUCCESS
-            OUTPUT_VARIABLE PYTHON_BASE_PREFIX
-        )
-        INSTALL(
-            DIRECTORY "${PYTHON_BASE_PREFIX}/"
-            DESTINATION "${install_dir}"
-            PATTERN "*test*" EXCLUDE
-        )
-        INSTALL(
-            DIRECTORY  "${VENV_PREFIX}/Lib/" 
-            DESTINATION "${install_dir}/Lib"
-            PATTERN "*test*" EXCLUDE
-        )
-    else ()
-        message(FATAL_ERROR "Please use a virtual env (venv or conda)")
-    endif ()
+function(getset_python_base_prefix)
+    execute_process(
+        COMMAND 
+            "${PYTHON_EXECUTABLE}" 
+            "-c" 
+            "import sys;print(sys.base_prefix, end='');"
+        OUTPUT_VARIABLE PYTHON_BASE_PREFIX
+    )
+    set(PYTHON_BASE_PREFIX "${PYTHON_BASE_PREFIX}" PARENT_SCOPE)
 endfunction()
 
+function(copy_python_venv install_dir)
+    install(
+        DIRECTORY "${PYTHON_BASE_PREFIX}/"
+        DESTINATION "${install_dir}"
+        PATTERN "*tests*" EXCLUDE
+    )
 
-function (copy_python_dll)
-    if (CONDA_PREFIX)
-        set(PYTHON_DLL "${PYTHON_PREFIX}/python${PYTHON_LIBRARY_SUFFIX}.dll")
-        install(FILES ${PYTHON_DLL} DESTINATION ${CLOUDCOMPARE_DEST_FOLDER})
-    elseif (DEFINED ENV{VIRTUAL_ENV})
-        execute_process(
-            COMMAND 
-                "${PYTHON_EXECUTABLE}" 
-                "-c" 
-                "from distutils import sysconfig as s;print(s.BASE_PREFIX, end='');"
-            RESULT_VARIABLE _PYTHON_SUCCESS
-            OUTPUT_VARIABLE PYTHON_BASE_PREFIX
+    if (NOT "${PYTHON_BASE_PREFIX}" STREQUAL "${PYTHON_PREFIX}")
+        install(
+            DIRECTORY "${PYTHON_PREFIX}/Lib/site-packages/"
+            DESTINATION "${install_dir}/Lib/site-packages/"
+            PATTERN "*tests*" EXCLUDE
         )
-        install(FILES "${PYTHON_BASE_PREFIX}/python${PYTHON_LIBRARY_SUFFIX}.dll" DESTINATION ${CLOUDCOMPARE_DEST_FOLDER})
     endif()
 endfunction()
 
 
-function(get_python_sites_packages PYTHON_SITE_PACKAGES)
-    execute_process(
-        COMMAND 
-            "${PYTHON_EXECUTABLE}" 
-            "-c"   
-            "from distutils import sysconfig as s;print(s.get_python_lib(plat_specific=True));"
-        RESULT_VARIABLE _PYTHON_SUCCESS
-        OUTPUT_VARIABLE _PYTHON_VALUES
-        ERROR_VARIABLE _PYTHON_ERROR_VALUE
+function (copy_python_dll)
+    install(
+        FILES "${PYTHON_BASE_PREFIX}/python${PYTHON_LIBRARY_SUFFIX}.dll" 
+        DESTINATION ${CLOUDCOMPARE_DEST_FOLDER}
     )
-
-    if (NOT _PYTHON_SUCCESS MATCHES 0)
-        message("${_PYTHON_SUCCESS}")
-        return()
-    endif ()
-
-    # Convert the process output into a list
-    if (WIN32)
-        string(REGEX REPLACE "\\\\" "/" _PYTHON_VALUES ${_PYTHON_VALUES})
-    endif ()
-
-    string(REGEX REPLACE "\n" "" _PYTHON_VALUES ${_PYTHON_VALUES})
-
-    set(${PYTHON_SITE_PACKAGES} ${_PYTHON_VALUES} PARENT_SCOPE)
 endfunction()
+
 
 function(manage_windows_install)
     set(CC_PYTHON_ENV_NAME "Python")
     set(CC_PLUGIN_INSTALL_DIR ${CLOUDCOMPARE_DEST_FOLDER}/plugins)
     set(CC_PYTHON_INSTALL_DIR "${CC_PLUGIN_INSTALL_DIR}/${CC_PYTHON_ENV_NAME}")
+
+    getset_python_base_prefix()
 
     if (PLUGIN_PYTHON_COPY_ENV)
         copy_python_venv(${CC_PYTHON_INSTALL_DIR})
@@ -85,21 +47,25 @@ function(manage_windows_install)
 
     copy_python_dll()
 
-    install(FILES "${CMAKE_CURRENT_SOURCE_DIR}/docs/stubfiles/pycc.pyi"
+    install(
+        FILES "${CMAKE_CURRENT_SOURCE_DIR}/docs/stubfiles/pycc.pyi"
                   "${CMAKE_CURRENT_SOURCE_DIR}/docs/stubfiles/cccorelib.pyi"
-            DESTINATION "${CC_PYTHON_INSTALL_DIR}/Lib/site-packages")
+        DESTINATION "${CC_PYTHON_INSTALL_DIR}/Lib/site-packages"
+    )
 
     if (NOT PLUGIN_PYTHON_USE_EMBEDDED_MODULES)
-        install(TARGETS pycc cccorelib
-                DESTINATION "${CC_PYTHON_INSTALL_DIR}/Lib/site-packages")
+        install(
+            TARGETS pycc cccorelib
+            DESTINATION "${CC_PYTHON_INSTALL_DIR}/Lib/site-packages"
+        )
     endif()
 
     # If the docs have been built, we copy them in the install folder
     set(DOCUMENTATION_FOLDER "${CMAKE_CURRENT_SOURCE_DIR}/docs/_build")
     if (EXISTS "${DOCUMENTATION_FOLDER}/index.html")
         install(
-                DIRECTORY "${DOCUMENTATION_FOLDER}/"
-                DESTINATION ${CC_PYTHON_INSTALL_DIR}/docs
+            DIRECTORY "${DOCUMENTATION_FOLDER}/"
+            DESTINATION ${CC_PYTHON_INSTALL_DIR}/docs
         )
     endif()
 endfunction()
