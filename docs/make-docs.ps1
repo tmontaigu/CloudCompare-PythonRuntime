8eab4321faadc64f71ddab085c317486af5e4cae
tmontaigu <thomas.montaigu@laposte.net>
2021-04-21 17:15:00 +0200

    test

--- docs/make-docs.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory = $true)]
-    [String]$CloudCompareExePath,
-    [string]$BuildType = "html",
-    [string]$OutputDir = "_build"
-)
-
-
-&"$CloudCompareExePath" -SILENT -PYTHON_SCRIPT build_doc.py -b $BuildType . $OutputDir
--- wrapper/cccorelib/cccorelib/__init__.py
+++
@@ -1,1 +0,0 @@
-from .cccorelib import *
