3605cdc53d9475dc5d27d97947222c50e73f098a
tmontaigu <thomas.montaigu@laposte.net>
2021-07-07 15:51:12 +0200

    I like progress bars

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from invoke import task
 from pathlib import Path
+from tqdm import tqdm
 
 import itertools
 CLANG_FORMAT_CMD = "clang-format -i {}"
@@ -8,14 +9,19 @@ CLANG_FORMAT_CMD = "clang-format -i {}"
 def clang_format(c):
     sources = Path('.').rglob("*.cpp")
     headers = Path('.').rglob("*.h")
-    for path in itertools.chain(sources, headers):
+
+    all_sources = list(itertools.chain(sources, headers))
+
+    print('Formating source files')
+    for path in tqdm(all_sources, ncols=80):
         c.run(f"clang-format -i {path}")
 
 
 @task
 def cmake_format(c):
-    cmakelists = Path('.').rglob('CMakeLists.txt')
-    for path in cmakelists:
+    cmakelists = list(Path('.').rglob('CMakeLists.txt'))
+    print('Formating CMakeLists.txt')
+    for path in tqdm(cmakelists, ncols=80):
         c.run(f'cmake-format -i {path}')
 
 
