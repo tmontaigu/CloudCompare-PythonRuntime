99d96fa574133e6180e177d8d14b02e19cb67d12
tmontaigu <thomas.montaigu@laposte.net>
2022-07-25 23:16:08 +0200

    Fix tasks.py

--- tasks.py
+++ tasks.py
@@ -1,16 +1,35 @@
 from invoke import task
 from pathlib import Path
 from tqdm import tqdm
+from typing import List
 
 import itertools
 CLANG_FORMAT_CMD = "clang-format -i {}"
+
+DISALLOW_LIST = ['_skbuild']
+
+
+def filter_paths(paths: List[Path]) -> List[Path]:
+    filtered_paths = []
+
+    for path in paths:
+        allowed = True
+        for p in DISALLOW_LIST:
+            if p in path.parts:
+                allowed = False
+
+        if allowed:
+            filtered_paths.append(path)
+
+    return filtered_paths
+
 
 @task
 def clang_format(c):
     sources = Path('.').rglob("*.cpp")
     headers = Path('.').rglob("*.h")
 
-    all_sources = list(itertools.chain(sources, headers))
+    all_sources = filter_paths(itertools.chain(sources, headers))
 
     print('Formating source files')
     for path in tqdm(all_sources, ncols=80):
@@ -20,8 +39,12 @@ def clang_format(c):
 @task
 def cmake_format(c):
     cmakelists = list(Path('.').rglob('CMakeLists.txt'))
+    cmakes = list(Path("./cmake").iterdir())
+
+    all_cmakes = filter_paths(list(itertools.chain(cmakes, cmakelists)))
+
     print('Formating CMakeLists.txt')
-    for path in tqdm(cmakelists, ncols=80):
+    for path in tqdm(all_cmakes, ncols=80):
         c.run(f'cmake-format -i {path}')
 
 
