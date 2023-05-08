b157e9710dd0464ddcbe55ea847d781e9ef2b8c4
tmontaigu <thomas.montaigu@laposte.net>
2023-05-08 11:56:34 +0200

    Add a sub menu for quick access to python plugins actions
    This commits adds a sub menu that containts the actions
    that python pllugin declare.
    
    Additionnally, actions and the plugin itself can now have icons
    
    Co-authored-by: romain janvier <romain.janvier@hotmail.fr>

--- script_examples/plugin_examples/countplugin/myplugin.py
+++ script_examples/plugin_examples/countplugin/myplugin.py
@@ -1,6 +1,11 @@
 import pycc
 
 from typing import List
+from pathlib import Path
+
+MAIN_ICON_PATH = str(Path(__file__).parent / "orange-square.png")
+COUNT_ICON_BYTES = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00 \x00\x00\x00 \x08\x06\x00\x00\x00szz\xf4\x00\x00\x00\tpHYs\x00\x00\x0e\xc4\x00\x00\x0e\xc4\x01\x95+\x0e\x1b\x00\x00\x00oIDATX\x85\xed\xd51\x0e\x80 \x10D\xd1\xeff\x0b\x0e`a\xe1a\xb9\xa1W1\x1c\xc0XjAB\xe3\xba&\xceT\x84f\x1fd\x08S\xa5n\xc0\x028`\\\xb9\xaf{9\x1e\xd8k\x0e\xac\xc0<\x18\x16\x952:ex\x8c\xf1U\x87\x03R#\x80: \x80\x00F\xff\xb3x\x15\x90\x1a\x01\x04\xd0+\x10\xe0\x13\x00\x95P\x80t\xc0\xbfK\xe8@\x03J\xd2\xfc\xfd\x04I\x84\r;\xd4\xf3>\xb5\x00\x00\x00\x00IEND\xaeB`\x82'
+SELECTED_ENTITIES_ICON_PATH = str(Path(__file__).parent / "blue-square.png")
 
 
 class CountPlugin(pycc.PythonPluginInterface):
@@ -9,10 +14,14 @@ class CountPlugin(pycc.PythonPluginInterface):
         self.count = 0
         self.app = pycc.GetInstance()
 
+    def getIcon(self):
+        return MAIN_ICON_PATH
+
     def getActions(self) -> List[pycc.Action]:
         return [
-            pycc.Action(name="Print Count", target=self.printCount),
-            pycc.Action(name="Print Selected Entities", target=self.printSelectedEntities),
+			# If no format is specified for the icon, Qt will try to guess is
+            pycc.Action(name="Print Count", target=self.printCount, icon=COUNT_ICON_BYTES),
+            pycc.Action(name="Print Selected Entities", target=self.printSelectedEntities, icon=SELECTED_ENTITIES_ICON_PATH),
         ]
 
     def printCount(self):
