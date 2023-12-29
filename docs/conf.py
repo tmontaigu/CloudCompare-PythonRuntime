f1d0fa64d82464e70eb14ca1b622585bf8810e7d
tmontaigu <thomas.montaigu@laposte.net>
2023-12-29 20:07:51 +0100

    Add cmake target to run doctests

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
     'sphinx.ext.inheritance_diagram',
     'breathe'
 ]
