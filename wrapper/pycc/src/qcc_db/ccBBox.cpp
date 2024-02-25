c5b83146a583368e4b16616bf65f67a5384a3e5b
tmontaigu <thomas.montaigu@laposte.net>
2024-02-25 18:14:57 +0100

    Add docs for ccBBox

--- wrapper/pycc/src/qcc_db/ccBBox.cpp
+++ wrapper/pycc/src/qcc_db/ccBBox.cpp
@@ -27,15 +27,35 @@ using namespace pybind11::literals;
 
 void define_ccBBox(py::module &m)
 {
-    py::class_<ccBBox, CCCoreLib::BoundingBox>(m, "ccBBox")
+    py::class_<ccBBox, CCCoreLib::BoundingBox>(m, "ccBBox", R"pbdoc(
+    Bounding box structure
+
+    supports multiplication (*) with :py:class:`pycc.ccGLMatrix` to apply a transformation.
+
+    Examples
+    --------
+
+    >>> import pycc
+    >>> emtpy_bbox = pycc.ccBBox()
+
+    >>> import cccorelib
+    >>> import pycc
+    >>> bb_min = cccorelib.CCVector3(0.0, 0.0, 0.0)
+    >>> bb_max = cccorelib.CCVector3(10.0, 15.0, 10.0)
+    >>> bbox = pycc.ccBBox(bb_min, bb_max)
+)pbdoc")
         .def(py::init<>())
         .def(py::init<const CCVector3 &, const CCVector3 &, bool>(),
              "bbMinCorner"_a,
              "bbMaxCorner"_a,
              "valid"_a)
         .def(
-            "__mul__", [](ccBBox &self, ccGLMatrix &mat) { return self * mat; }, py::is_operator())
+            "__mul__",
+            [](ccBBox &self, const ccGLMatrix &mat) { return self * mat; },
+            py::is_operator())
         .def(
-            "__mul__", [](ccBBox &self, ccGLMatrixd &mat) { return self * mat; }, py::is_operator())
+            "__mul__",
+            [](ccBBox &self, const ccGLMatrixd &mat) { return self * mat; },
+            py::is_operator())
         .def("draw", &ccBBox::draw, "context"_a, "col"_a);
 }
