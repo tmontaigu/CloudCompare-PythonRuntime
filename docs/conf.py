214ff25bd828594d4462731f06d886e9fc740a9e
tmontaigu <thomas.montaigu@laposte.net>
2024-08-17 17:15:26 +0200

    remove cpp functions from docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,12 +36,6 @@ extensions = [
     'sphinx.ext.inheritance_diagram',
     'breathe'
 ]
-breathe_projects = {
-    "CCCoreLib": r"__build/CCCoreLib/xml",
-    "qCC_db": r"__build/qCC_db/xml"
-}
-
-breathe_default_project = "qCC_db"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
